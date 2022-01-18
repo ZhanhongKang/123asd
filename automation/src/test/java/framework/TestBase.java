eee1d692d384ace57d96f7c7efc0e49c90493cec
Shawn <Shawn@Shawn-PC.lan>
2022-01-18 10:41:11 -0500

    upload to github

--- automation/src/test/java/framework/TestBase.java
+++ automation/src/test/java/framework/TestBase.java
@@ -2,30 +2,33 @@ package framework;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 
 public abstract class TestBase {
-  
+
 	protected String baseUrl;
 	protected ChromeDriver driver ;
-	
-  @BeforeMethod
-  public void setup() {
-	  baseUrl = "http://automationpractice.com/index.php";
-	  System.setProperty("webdriver.chrome.driver", "C:\\Users\\Shawn\\eclipse-workspace\\automation\\src\\test\\resources\\chromedrivers.exe");
-	   driver = new ChromeDriver();
-	   
-	   driver.manage().window().maximize();
-	   
-	   driver.get(baseUrl);
-	   
-  }
 
-  @AfterMethod
-  public void cleanup() {
-	  driver.close();
-  }
+	@BeforeMethod
+	public void setup() {
+		baseUrl = "http://automationpractice.com/index.php";
+		System.setProperty("webdriver.chrome.driver", ".\\src\\test\\resources\\chromedrivers.exe");
+		driver = new ChromeDriver();
+
+		driver.manage().window().maximize();
+
+		driver.get(baseUrl);
+		driver.manage().timeouts().implicitlyWait(6, TimeUnit.SECONDS);
+	}
+
+	@AfterMethod
+	public void cleanup() {
+		driver.close();
+	}
 
 }
--- automation/src/test/java/test/login.java
+++ automation/src/test/java/test/login.java
@@ -3,9 +3,13 @@ package test;
 import org.testng.annotations.Test;
 
 import PageObject.HomePage;
+import PageObject.blousePage;
 import framework.TestBase;
 
 import org.testng.annotations.BeforeMethod;
+
+import static org.testng.Assert.assertEquals;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,12 +17,37 @@ import org.testng.annotations.AfterMethod;
 
 public class login extends TestBase{
 
-	
-  @Test
-  public void fasdasd() throws InterruptedException {
-	 new HomePage(driver,baseUrl).addtocartTshits();
-	  
-  }
-  
 
+	@Test
+	public void fasdasd() throws InterruptedException {
+		new HomePage(driver,baseUrl).addtocartTshits();
+
+	}
+
+	@Test
+	public void searchBlouseProduct() {
+		String expect = "Product successfully added to your shopping cart";
+		
+		blousePage actural = new HomePage(driver,baseUrl)
+				.runSearch()
+				.selectItem()
+				.addToCart();
+		
+		assertEquals(expect, actural.grabText());	
+	
+
+	}
+	
+	@Test
+	public void searchBlouseProduct1() {
+		String expect = "There is 1 item in your cart.";
+		
+		blousePage actural = new HomePage(driver,baseUrl)
+				.runSearch()
+				.selectItem()
+				.addToCart();
+		
+		assertEquals(expect, actural.grabText1());	
+
+	}
 }
